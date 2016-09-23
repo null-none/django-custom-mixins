19e0544c5c822d3451c9a0432e34a69ee0fc38a3
Dmitry <dmitry.kalinin.email@gmail.com>
2016-09-23 22:38:57 +0300

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-custom-mixins',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Dmitry Kalinin',
     author_email='dmitry.kalinin.email@gmail.com',
