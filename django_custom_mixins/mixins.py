337d39d466914be2e4eb3ab22563e60d9c03f4c6
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2019-02-26 10:48:53 +0200

    Update mixins.py

--- django_custom_mixins/mixins.py
+++ django_custom_mixins/mixins.py
@@ -13,7 +13,47 @@ from django.utils import timezone
 from django.forms.models import model_to_dict
 
 import pytz
+import threading
 
+class ModelAdminRequestMixin(object):
+    def __init__(self, *args, **kwargs):
+        # let's define this so there's no chance of AttributeErrors
+        self._request_local = threading.local()
+        self._request_local.request = None
+        super(ModelAdminRequestMixin, self).__init__(*args, **kwargs)
+
+    def get_request(self):
+        return self._request_local.request
+
+    def set_request(self, request):
+        self._request_local.request = request
+
+    def changeform_view(self, request, *args, **kwargs):
+        # stash the request
+        self.set_request(request)
+
+        # call the parent view method with all the original args
+        return super(ModelAdminRequestMixin, self).changeform_view(request, *args, **kwargs)
+
+    def add_view(self, request, *args, **kwargs):
+        self.set_request(request)
+        return super(ModelAdminRequestMixin, self).add_view(request, *args, **kwargs)
+
+    def change_view(self, request, *args, **kwargs):
+        self.set_request(request)
+        return super(ModelAdminRequestMixin, self).change_view(request, *args, **kwargs)
+
+    def changelist_view(self, request, *args, **kwargs):
+        self.set_request(request)
+        return super(ModelAdminRequestMixin, self).changelist_view(request, *args, **kwargs)
+
+    def delete_view(self, request, *args, **kwargs):
+        self.set_request(request)
+        return super(ModelAdminRequestMixin, self).delete_view(request, *args, **kwargs)
+
+    def history_view(self, request, *args, **kwargs):
+        self.set_request(request)
+        return super(ModelAdminRequestMixin, self).history_view(request, *args, **kwargs)
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
